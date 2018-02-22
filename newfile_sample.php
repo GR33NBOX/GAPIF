c51af87fd5c23e6b4f2a888ef05876d0e29f2d5c
SMTushar <you@example.com>
2018-02-22 19:12:08 +0600

    commit

---
+++ newfile_sample.php
@@ -0,0 +1,18 @@
+<?php
+
+/*@Green Api Framework [An Open Source API Framework Based On PHP]
+ *@Developer Agency: Green Box
+ *@Publisher Agency: Open Source Solver
+ *@Author: Green Box & Open Source Solver
+ *@Copyright: Green Box & Open Source Solver
+ *@Copyright Under: http://creativecommons.org/licenses/by-nc-sa/4.0/
+ *@Contact: gr33nbox.work@gmail.com, www.gr33nbox.work
+ *@Address: Dhaka, Bangladesh
+ */
+
+/*@FileName: .............filename.php.............
+ *@FileCreator: .............creator name.............
+ *@Comments: .............comments.............
+ */
+//START//
+// .............Your PHP code here...............
